05c706870651f927129cba3b2f711e5e649d2f28
Georgi Cholakov <cholakovge@gmail.com>
2020-12-27 15:31:58 +0200

    auth guard refactored

--- front-end/exclu-cv/src/app/Authentication/auth.guard.ts
+++ front-end/exclu-cv/src/app/Authentication/auth.guard.ts
@@ -1,23 +1,24 @@
 import { Injectable } from '@angular/core';
-import { Router, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import {
+  Router,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+} from '@angular/router';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthGuard {
-
-  constructor(private router: Router) {
-  }
+  constructor(private router: Router) {}
 
   canActivate(
     next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): boolean {
-    if (localStorage.getItem('token') != null)
-      return true;
+    state: RouterStateSnapshot
+  ): boolean {
+    if (localStorage.getItem('token') != null) return true;
     else {
-      this.router.navigate(['/user/login']);
+      this.router.navigate(['']);
       return false;
     }
-
   }
 }
