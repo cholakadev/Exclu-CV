ca9585746939a89e496476b3fe20acf8b4994bd7
Georgi Cholakov <cholakovge@gmail.com>
2021-01-23 21:13:30 +0200

    Education form added in form array

--- front-end/exclu-cv/src/app/create-new-cv/pages/education/education.component.ts
+++ front-end/exclu-cv/src/app/create-new-cv/pages/education/education.component.ts
@@ -1,32 +1,35 @@
 import { Component, OnInit } from '@angular/core';
-import { FormArray, FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
+import { FormArray, FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { GlobalConstants } from 'src/environments/environment';
 import { ToastrService } from 'ngx-toastr';
-import { MomentDateAdapter, MAT_MOMENT_DATE_ADAPTER_OPTIONS } from '@angular/material-moment-adapter';
-import { DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE } from '@angular/material/core';
-import { MatDatepicker } from '@angular/material/datepicker';
 import * as _moment from 'moment';
-import { default as _rollupMoment, Moment } from 'moment';
+import { default as _rollupMoment } from 'moment';
 
 @Component({
   selector: 'app-education',
   templateUrl: './education.component.html',
   styleUrls: ['./education.component.scss'],
 })
-
 export class EducationComponent implements OnInit {
+  public educations: FormArray;
+  public educationForm: FormGroup;
 
-  educationForm: FormGroup;
   submitted = false;
 
-
-  constructor(
-    private formBuilder: FormBuilder,
-    private toastr: ToastrService,
-  ) { }
-
-  ngOnInit(): void {
+  constructor(private formBuilder: FormBuilder, private toastr: ToastrService) {
     this.educationForm = this.formBuilder.group({
+      educations: this.formBuilder.array([]),
+    });
+  }
+
+  ngOnInit(): void {}
+
+  get educationControls() {
+    return this.educationForm.get('educations')['controls'];
+  }
+
+  createEducation(): FormGroup {
+    return this.formBuilder.group({
       start_date: ['', Validators.required],
       end_date: ['', Validators.required],
       institution: ['', Validators.required],
@@ -34,7 +37,19 @@ export class EducationComponent implements OnInit {
     });
   }
 
-  get f() { return this.educationForm.controls; }
+  addEducation(): void {
+    this.educations = this.educationForm.get('educations') as FormArray;
+    this.educations.push(this.createEducation());
+  }
+
+  removeEducation(i: number) {
+    this.educations.removeAt(i);
+  }
+
+  logValue() {
+    console.log('logValue');
+    console.log(this.educations.value);
+  }
 
   onSubmit(data): any {
     this.submitted = true;
@@ -42,9 +57,7 @@ export class EducationComponent implements OnInit {
       return;
     }
 
-    GlobalConstants.cv['educations'] = data;
-    localStorage.setItem("cv", JSON.stringify(GlobalConstants.cv))
-    this.toastr.success('Successfully saved!');
+    console.log(data);
   }
 
   get educationForms() {
@@ -53,8 +66,7 @@ export class EducationComponent implements OnInit {
 
   addNewEducationForm() {
     // this.educationForms.push(this.formBuilder.control(''));
-    let cv = localStorage.getItem("cv");
+    let cv = localStorage.getItem('cv');
     console.log(cv);
   }
-
 }
