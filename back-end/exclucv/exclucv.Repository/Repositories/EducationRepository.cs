7925f277470dae8862b534ce4330a5900d2c7964
Georgi Cholakov <cholakovge@gmail.com>
2021-01-23 22:07:21 +0200

    Education repository initial create

---
+++ back-end/exclucv/exclucv.Repository/Repositories/EducationRepository.cs
@@ -0,0 +1,20 @@
+﻿namespace exclucv.Repository.Repositories
+{
+    using exclucv.DAL.Entities;
+    using exclucv.Repository.RepositoryContracts;
+    using System;
+    using System.Collections.Generic;
+
+    public class EducationRepository : IEducationRepository
+    {
+        public void AddEducations(List<Education> educations)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Guid RemoveEducation(Guid educationId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ back-end/exclucv/exclucv.Repository/RepositoryContracts/IEducationRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace exclucv.Repository.RepositoryContracts
+{
+    using exclucv.DAL.Entities;
+    using System;
+    using System.Collections.Generic;
+
+    public interface IEducationRepository
+    {
+        void AddEducations(List<Education> educations);
+        Guid RemoveEducation(Guid educationId);
+    }
+}
